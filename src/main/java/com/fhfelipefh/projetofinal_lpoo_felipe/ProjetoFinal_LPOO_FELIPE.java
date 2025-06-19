4ec7d55a3f61fa1c699ba7021dd41d8e38629bf2
FELIPE HONORIO MORAES <felipehonorio55@gmail.com>
2025-06-19 13:16:08 -0300

    Redimensionamento do software automático

--- src/main/java/com/fhfelipefh/projetofinal_lpoo_felipe/ProjetoFinal_LPOO_FELIPE.java
+++ src/main/java/com/fhfelipefh/projetofinal_lpoo_felipe/ProjetoFinal_LPOO_FELIPE.java
@@ -4,6 +4,10 @@
 
 package com.fhfelipefh.projetofinal_lpoo_felipe;
 
+import com.fhfelipefh.projetofinal_lpoo_felipe.view.MainForm;
+
+import javax.swing.*;
+
 /**
  *
  * @author 20201PF.CC0181
@@ -12,6 +16,14 @@ public class ProjetoFinal_LPOO_FELIPE {
 
     public static void main(String[] args) {
         System.out.println("Projeto Final LPOO - Felipe");
-
+        SwingUtilities.invokeLater(() -> {
+            MainForm form = new MainForm();
+            JFrame frame = new JFrame("Reservas de Salas");
+            frame.setContentPane(form.getRootComponent());
+            frame.pack();
+            frame.setSize(1024, 768);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
     }
 }
--- src/main/java/com/fhfelipefh/projetofinal_lpoo_felipe/view/MainForm.java
+++ src/main/java/com/fhfelipefh/projetofinal_lpoo_felipe/view/MainForm.java
@@ -1,10 +1,22 @@
 package com.fhfelipefh.projetofinal_lpoo_felipe.view;
 
-import com.github.lgooddatepicker.components.CalendarPanel;
-import com.toedter.calendar.JCalendar;
-
 import javax.swing.*;
 
 public class MainForm {
-    private JTabbedPane tabbedPane1;
+    private JTabbedPane tabbedPaneMain;
+    private JSplitPane splitPanelVertical;
+
+    public MainForm() {
+        splitPanelVertical.setLeftComponent(null);
+        splitPanelVertical.setRightComponent(null);
+        splitPanelVertical.setResizeWeight(0.5);
+        SwingUtilities.invokeLater(() ->
+                splitPanelVertical.setDividerLocation(0.5)
+        );
+    }
+
+    public JComponent getRootComponent() {
+        return tabbedPaneMain;
+    }
+
 }
