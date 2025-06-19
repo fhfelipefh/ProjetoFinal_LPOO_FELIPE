3259fc9587afbe128eada5920291e23b96517b57
FELIPE HONORIO MORAES <felipehonorio55@gmail.com>
2025-06-19 17:31:17 -0300

    Update MainForm.java

--- src/main/java/com/fhfelipefh/projetofinal_lpoo_felipe/view/MainForm.java
+++ src/main/java/com/fhfelipefh/projetofinal_lpoo_felipe/view/MainForm.java
@@ -1,24 +1,43 @@
 package com.fhfelipefh.projetofinal_lpoo_felipe.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainForm {
+    private JPanel root;
     private JTabbedPane tabbedPaneMain;
 
     public MainForm() {
+        root = new JPanel(new BorderLayout());
         tabbedPaneMain = new JTabbedPane();
 
-        ReservasForm reservasForm = new ReservasForm();
-        tabbedPaneMain.addTab("Reservas", reservasForm);
+        tabbedPaneMain.addTab("Reservas", new ReservasForm());
+        tabbedPaneMain.addTab("Salas", new SalasForm());
+        tabbedPaneMain.addTab("Usuários", new UsuariosForm());
 
-        JPanel salasPanel = new SalasForm();
-        tabbedPaneMain.addTab("Salas", salasPanel);
+        JMenuBar bar = new JMenuBar();
+        JMenu help = new JMenu("Ajuda");
+        JMenuItem sobre = new JMenuItem("Sobre");
+        sobre.addActionListener(e ->
+                JOptionPane.showMessageDialog(
+                        root,
+                        """
+                                Sistema de Reservas de Salas
+                                
+                                Projeto acadêmico da disciplina LPOO
+                                que gerencia cadastros de salas, usuários
+                                e reservas, controlando conflitos de horário
+                                e status das solicitações.
+                                """,
+                        "Sobre", JOptionPane.INFORMATION_MESSAGE));
+        help.add(sobre);
+        bar.add(help);
 
-        JPanel usuariosPanel = new UsuariosForm();
-        tabbedPaneMain.addTab("Usuários", usuariosPanel);
+        root.add(bar, BorderLayout.NORTH);
+        root.add(tabbedPaneMain, BorderLayout.CENTER);
     }
 
     public JComponent getRootComponent() {
-        return tabbedPaneMain;
+        return root;
     }
 }
